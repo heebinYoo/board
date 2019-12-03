f156b5a4a105a7c9a4e703660c2ae67cf8da291b
cau <cau@cau>
2019-12-04 08:59:13 +0900

    중간과정3

---
+++ src/main/java/bean/Coord.java
@@ -0,0 +1,19 @@
+package bean;
+
+public class Coord {
+    private int row;
+    private int col;
+
+    public Coord(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+}
