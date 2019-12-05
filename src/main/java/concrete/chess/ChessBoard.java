8352e0430dd70a36762e1c5fa58b03de42bf59de
Administrator <asdf>
2019-12-05 19:07:09 +0900

    merge stm

--- src/main/java/concrete/chess/ChessBoard.java
+++ src/main/java/concrete/chess/ChessBoard.java
@@ -29,22 +29,44 @@ public class ChessBoard extends Board {
 
     @Override
     public void init() {
+
+
+
         super.pieceData = new Piece[8][8];//row, col
         PieceFactory pieceFactory = new ConcretePieceFactory();
 
         //make piceData
         //for player 1 and player 2
         //put in pieceData, in appropriate place
-        Piece piece = pieceFactory.createPiece(BLACK, ChessPieceEnum.rukh);
-        super.pieceData[0][0] = piece;
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        super.pieceData[0][0] = pieceFactory.createPiece(BLACK, ChessPieceEnum.rukh);
+        super.pieceData[0][1] = pieceFactory.createPiece(BLACK, ChessPieceEnum.knight);
+        super.pieceData[0][2] = pieceFactory.createPiece(BLACK, ChessPieceEnum.bishop);
+        super.pieceData[0][3] = pieceFactory.createPiece(BLACK, ChessPieceEnum.king);
+        super.pieceData[0][4] = pieceFactory.createPiece(BLACK, ChessPieceEnum.queen);
+        super.pieceData[0][5] = pieceFactory.createPiece(BLACK, ChessPieceEnum.bishop);
+        super.pieceData[0][6] = pieceFactory.createPiece(BLACK, ChessPieceEnum.rukh);
+
+        for(int i = 0; i < 7 ; i++) {
+            super.pieceData[1][i] = pieceFactory.createPiece(BLACK, ChessPieceEnum.pawn);
+        }
+
+
+        super.pieceData[7][0] = pieceFactory.createPiece(WHITE, ChessPieceEnum.rukh);
+        super.pieceData[7][1] = pieceFactory.createPiece(WHITE, ChessPieceEnum.knight);
+        super.pieceData[7][2] = pieceFactory.createPiece(WHITE, ChessPieceEnum.bishop);
+        super.pieceData[7][3] = pieceFactory.createPiece(WHITE, ChessPieceEnum.king);
+        super.pieceData[7][4] = pieceFactory.createPiece(WHITE, ChessPieceEnum.queen);
+        super.pieceData[7][5] = pieceFactory.createPiece(WHITE, ChessPieceEnum.bishop);
+        super.pieceData[7][6] = pieceFactory.createPiece(WHITE, ChessPieceEnum.rukh);
+
+        for(int i = 0; i < 7 ; i++) {
+            super.pieceData[6][i]=pieceFactory.createPiece(WHITE, ChessPieceEnum.pawn);
+        }
 
         //make observers
         super.add(new CastlingObserver());
         super.add(new CheckmateObserver());
         super.add(new EnPassantObserver());
-
-
     }
 
     //recursivily called
