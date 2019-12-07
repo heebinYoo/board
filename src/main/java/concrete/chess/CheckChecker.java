5668dc2c88b9d823dfb06581ed70af88980793c7
cau <cau@cau>
2019-12-08 02:28:52 +0900

    edit CheckChecker.isCheck

--- src/main/java/concrete/chess/CheckChecker.java
+++ src/main/java/concrete/chess/CheckChecker.java
@@ -16,26 +16,23 @@ public class CheckChecker {
     private ConcreteMoveCheckerFactory moveCheckerFactory = new ConcreteMoveCheckerFactory();
 
     public boolean isCheck(Piece king, Coord destination){
-        if ((BoardManager.getInstance().getBoardInstance().getPieceOn(destination) != null)&&(BoardManager.getInstance().getBoardInstance().getPieceOn(destination).getType()== ChessPieceEnum.king)){
-            //null exception, king check
-            ArrayList<Coord> EnemyPiece = new ArrayList<Coord>();
-            for(int i = 0;i<8;i++){
-                for(int j = 0;j<8;j++){
-                    Coord allBoard = new Coord(i,j);
-                    if((BoardManager.getInstance().getBoardInstance().getPieceOn(allBoard)!=null)&&(king.getPlayer()!= BoardManager.getInstance().getBoardInstance().getPieceOn(allBoard).getPlayer())){
-                        //enemy check, add enemy piece coord to arraylist
-                        EnemyPiece.add(allBoard);
-                    }
+        ArrayList<Coord> EnemyPiece = new ArrayList<Coord>();
+        for(int i = 0;i<8;i++){
+            for(int j = 0;j<8;j++){
+                Coord allBoard = new Coord(i,j);
+                if((BoardManager.getInstance().getBoardInstance().getPieceOn(allBoard)!=null)&&(king.getPlayer()!= BoardManager.getInstance().getBoardInstance().getPieceOn(allBoard).getPlayer())){
+                    //enemy check, add enemy piece coord to arraylist
+                    EnemyPiece.add(allBoard);
                 }
             }
+        }
 
-            for(int i = 0;i<EnemyPiece.size();i++){
-                ArrayList<Coord> CheckOrNot = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(EnemyPiece.get(i))).getMoveableList(EnemyPiece.get(i));
-                for(int j = 0;j<CheckOrNot.size();j++){
-                    if(CheckOrNot.get(j)==destination){
-                        //checked
-                        return true;
-                    }
+        for(int i = 0;i<EnemyPiece.size();i++){
+            ArrayList<Coord> CheckOrNot = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(EnemyPiece.get(i))).getMoveableList(EnemyPiece.get(i));
+            for(int j = 0;j<CheckOrNot.size();j++){
+                if(CheckOrNot.get(j)==destination){
+                    //checked
+                    return true;
                 }
             }
         }
