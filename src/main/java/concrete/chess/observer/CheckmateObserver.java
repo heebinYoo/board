703c0a47ee7924dc96b3cf2eb872e595183c6508
JooSeuYoon <ajanajan1217@gmail.com>
2019-12-08 02:01:38 +0900

    CheckMate Modified

--- src/main/java/concrete/chess/observer/CheckmateObserver.java
+++ src/main/java/concrete/chess/observer/CheckmateObserver.java
@@ -17,45 +17,28 @@ public class CheckmateObserver implements Observer {
     public void update(Coord prev, Coord post) {
         if ((BoardManager.getInstance().getBoardInstance().getPieceOn(post) != null)&&(BoardManager.getInstance().getBoardInstance().getPieceOn(prev)!=null)){
             if (!(BoardManager.getInstance().getBoardInstance().getPieceOn(post).getType() == ChessPieceEnum.king)) {
-
-                ArrayList<Coord> isKingThere = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(post)).getMoveableList(post);
+                CheckChecker check = new CheckChecker();
                 //checked check
-                //if(CheckChecker.isCheck(BoardManager.getInstance().getBoardInstance().getPieceOn(post),post))
-                for (int i = 0; i < isKingThere.size(); i++) {
-                    if(BoardManager.getInstance().getBoardInstance().getPieceOn(isKingThere.get(i))!=null) {
-                        if (BoardManager.getInstance().getBoardInstance().getPieceOn(isKingThere.get(i)).getType() == ChessPieceEnum.king) {
-                            ArrayList<Coord> KingLoc = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(isKingThere.get(i))).getMoveableList(isKingThere.get(i));
-                            for (int j = 0; j < KingLoc.size(); j++) {
-                                for (int k = 0; k < 8; k++) {
-                                    for (int l = 0; l < 8; l++) {
-                                        Coord BoardCoord = new Coord(k, l);
-                                        if ((BoardManager.getInstance().getBoardInstance().getPieceOn(BoardCoord) != null)&&(BoardManager.getInstance().getBoardInstance().getPieceOn(KingLoc.get(j))!=null)) {
-                                            if (BoardManager.getInstance().getBoardInstance().getPieceOn(BoardCoord).getPlayer() != BoardManager.getInstance().getBoardInstance().getPieceOn(KingLoc.get(j)).getPlayer()) {
-                                                ArrayList<Coord> CheckedOrNot = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(BoardCoord)).getMoveableList(BoardCoord);
-                                                for (int m = 0; m < KingLoc.size(); m++) {
-                                                    for (int n = 0; n < CheckedOrNot.size(); n++) {
-                                                        if (KingLoc.get(m) == CheckedOrNot.get(n)) {
-                                                            //Cannot move to here
-                                                            KingLoc.remove(m);
-                                                            break;
-                                                        }
-                                                    }
-                                                }
-                                            }
+                if(check.isCheck(BoardManager.getInstance().getBoardInstance().getPieceOn(post),post)) {
+                    ArrayList<Coord> KingMove = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(post)).getMoveableList(post);
+                    for(int i = 0;i<8;i++){
+                        for(int j = 0;j<8;j++){
+                            Coord Board = new Coord(i,j);
+                            if((BoardManager.getInstance().getBoardInstance().getPieceOn(Board)!=null)&&(BoardManager.getInstance().getBoardInstance().getPieceOn(Board).getPlayer()!=BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer())){
+                                ArrayList<Coord> EnemyMove = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(Board)).getMoveableList(Board);
+                                for(int k = 0;k<EnemyMove.size();k++){
+                                    for(int l = 0;l<KingMove.size();l++){
+                                        if(EnemyMove.get(k)==KingMove.get(l)){
+                                            //CheckMate!!! exit
+                                            boardEventListner.onGameOver();
                                         }
                                     }
                                 }
-                            }
 
-                            if (KingLoc.size() == 0) {
-                                //CheckMate!!! exit
-                                boardEventListner.onGameOver();
                             }
-
                         }
                     }
                 }
-
             }
 
         }
