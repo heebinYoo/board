7e7d02fc80a338658b96691af6bc676add8805ce
JooSeuYoon <ajanajan1217@gmail.com>
2019-12-07 18:08:07 +0900

    EnPassant implemented without killing Castling modified

--- src/main/java/concrete/chess/observer/EnPassantObserver.java
+++ src/main/java/concrete/chess/observer/EnPassantObserver.java
@@ -5,18 +5,36 @@ import board.BoardManager;
 import concrete.ConcreteMoveCheckerFactory;
 import concrete.chess.piece.ChessPieceEnum;
 import history.History;
+import history.Record;
 import observer.Observer;
+
+import java.util.Iterator;
 
 public class EnPassantObserver implements Observer {
     private ConcreteMoveCheckerFactory moveCheckerFactory = new ConcreteMoveCheckerFactory();
     @Override
     public void update(Coord prev, Coord post) {
+        Iterator<Record> it = History.getInstance().iterator();
         if(BoardManager.getInstance().getBoardInstance().getPieceOn(post).getType() == ChessPieceEnum.pawn){
-            if(History.getInstance().getLast().getPost().getCol()==(BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer()*(-1)+5)){
-                //check enemy
-
+            //움직인게 pawn이라면
+            if((prev.getCol()==History.getInstance().getLast().getPost().getCol())&&(post.getRow()==History.getInstance().getLast().getPost().getRow())
+                    &&(Math.abs(post.getCol()-History.getInstance().getLast().getPost().getCol())==1)) {
+                //내 pawn의 움직이기 직전 col좌표와 이전 적의 col좌표가 같고
+                //내 pawn의 움직인 후의 row좌표와 이전 적의 row좌표가 같고
+                //내 pawn의 움직인 후의 col좌표와 이전 적의 col좌표의 차이가 1일때
+                if(Math.abs(History.getInstance().getLast().getPrev().getCol()-History.getInstance().getLast().getPrev().getCol())==2) {
+                    int i = 0;
+                    while (it.hasNext()) {
+                        if (it.next().getPiece().getId().equals(History.getInstance().getLast().getPiece().getId())) {
+                            i++;
+                        }
+                    }
+                    if(i==1){
+                        //EnPassant Check
+                        //TODO
+                    }
+                }
             }
-
         }
 
     }
