e567de5dcf74c7454b2b38211a01486b5f849b16
wonawonana <56299516+wonawonana@users.noreply.github.com>
2019-12-07 20:47:31 +0900

    solve king get problem

--- src/main/java/concrete/chess/moveChecker/KingMoveChecker.java
+++ src/main/java/concrete/chess/moveChecker/KingMoveChecker.java
@@ -4,6 +4,8 @@ import bean.Coord;
 import board.BoardManager;
 import concrete.ConcreteMoveCheckerFactory;
 import concrete.chess.piece.ChessPieceEnum;
+import controller.BoardEventListner;
+import exception.InvaildMoveException;
 import history.History;
 import history.Record;
 import moveChecker.MoveChecker;
@@ -79,53 +81,81 @@ public class KingMoveChecker implements MoveChecker {
 
     // //추가 함수. prev->post 로 킹이 움직일 수 있는지
     public boolean update(Coord prev, Coord post) {
-        //prev->post 로 가려고 한다
         ConcreteMoveCheckerFactory moveCheckerFactory = new ConcreteMoveCheckerFactory();
-        if (BoardManager.getInstance().getBoardInstance().getPieceOn(post).getType() == ChessPieceEnum.king) {
-            Iterator<Record> it = History.getInstance().iterator();
-            while (it.hasNext()) {
-                if ((it.next().getPiece().getPlayer() == BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer()) &&
-                        (it.next().getPiece().getType() == ChessPieceEnum.king))
-                    return false; //king moved before -> not castling
-                if ((it.next().getPiece().getPlayer() == BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer()) &&
-                        (it.next().getPiece().getType() == ChessPieceEnum.rukh))
-                    return false; //rukh moved before -> not castling
-            }
+        boolean rukh1 = false, rukh2 = false;
 
-            ArrayList<Coord> KingsWay = new ArrayList<>();
+        if ((BoardManager.getInstance().getBoardInstance().getPieceOn(post) != null)&&(BoardManager.getInstance().getBoardInstance().getPieceOn(prev)!=null)){
+            if ((BoardManager.getInstance().getBoardInstance().getPieceOn(post).getType() == ChessPieceEnum.king) && (Math.abs(prev.getRow() - post.getRow()) == 2)) {
+                if (History.getInstance().getLast() != null) {
+                    Iterator<Record> it = History.getInstance().iterator();
+                    while (it.hasNext()) {
+                        if ((it.next().getPiece().getPlayer() == BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer()) &&
+                                (it.next().getPiece().getType() == ChessPieceEnum.king))
+                            return false; //king moved before -> not castling
 
-            if ((prev.getCol() - post.getCol()) > 0) {
-                for (int j = 1; j < 3; j++) {
-                    Coord way = new Coord(BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer() * 7 - 7, j);
-                    KingsWay.add(way);
-                }
-            } else {
-                for (int j = 4; j < 7; j++) {
-                    Coord way = new Coord(BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer() * 7 - 7, j);
-                    KingsWay.add(way);
-                }
-            }
-
-            for (int i = 0; i < KingsWay.size(); i++) {
-                if (BoardManager.getInstance().getBoardInstance().getPieceOn(KingsWay.get(i)) != null) {
-                    return false; //there's other piece between king and rukh -> not castling
-                }
-            }
-
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 8; j++) {
-                    Coord Board = new Coord(i, j);
-                    ArrayList<Coord> obs = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(Board)).getMoveableList(Board);
-                    for (int k = 0; k < obs.size(); k++) {
-                        for (int l = 0; l < KingsWay.size(); l++) {
-                            if (obs.get(k) == KingsWay.get(l)) {
-                                return false; //there will be some attack to king -> not castling
-                            }
-                            if (obs.get(k) == post) {
-                                return false; //there will be some attack to king -> not castling
-                            }
+                        if ((it.next().getPiece().getPlayer() == BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer()) &&
+                                (it.next().getPiece().getId().contains("rukh") && it.next().getPiece().getId().contains("0"))) {
+                            //id 0 rukh moved before -> not castling
+                            rukh1 = true;
+                        } else if ((it.next().getPiece().getPlayer() == BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer()) &&
+                                (it.next().getPiece().getId().contains("rukh") && it.next().getPiece().getId().contains("1"))) {
+                            //id 1 rukh moved before -> not castling
+                            rukh2 = true;
                         }
                     }
+
+                    if (rukh1 && rukh2) {
+                        //two rukh moved before -> not castling
+                        return false;
+                    } else {
+
+                        ArrayList<Coord> KingsWay = new ArrayList<>();
+
+                        if ((prev.getCol() - post.getCol()) > 0) {
+                            for (int i = 1; i < 3; i++) {
+                                Coord way = new Coord(BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer() * 7 - 7, i);
+                                KingsWay.add(way);
+                            }
+                        } else {
+                            for (int i = 4; i < 7; i++) {
+                                Coord way = new Coord(BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer() * 7 - 7, i);
+                                KingsWay.add(way);
+                            }
+                        }
+
+                        for (int i = 0; i < KingsWay.size(); i++) {
+                            if (BoardManager.getInstance().getBoardInstance().getPieceOn(KingsWay.get(i)) != null) {
+                                return false; //there's other piece between king and rukh -> not castling
+                            }
+                        }
+
+                        for (int i = 0; i < 8; i++) {
+                            for (int j = 0; j < 8; j++) {
+                                Coord Board = new Coord(i, j);
+                                if (BoardManager.getInstance().getBoardInstance().getPieceOn(Board) != null) {
+                                    if (BoardManager.getInstance().getBoardInstance().getPieceOn(Board).getPlayer() != BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer()) {
+                                        ArrayList<Coord> obs = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(Board)).getMoveableList(Board);
+                                        for (int k = 0; k < obs.size(); k++) {
+                                            for (int l = 0; l < KingsWay.size(); l++) {
+                                                if (obs.get(k) == KingsWay.get(l)) {
+                                                    return false; //there will be some attack to king -> not castling
+                                                }
+                                                if (obs.get(k) == post) {
+                                                    return false; //there will be some attack to king -> not castling
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        //can castling
+                        //change rukh
+
+
+                    }
+
                 }
             }
 
@@ -134,43 +164,53 @@ public class KingMoveChecker implements MoveChecker {
     }
 
     public boolean update2(Coord prev, Coord post) {
-        ConcreteMoveCheckerFactory moveCheckerFactory = new ConcreteMoveCheckerFactory();
-        if(!(BoardManager.getInstance().getBoardInstance().getPieceOn(post).getType() == ChessPieceEnum.king)){
+      ConcreteMoveCheckerFactory moveCheckerFactory = new ConcreteMoveCheckerFactory();
+      BoardEventListner boardEventListner;
+        if ((BoardManager.getInstance().getBoardInstance().getPieceOn(post) != null)&&(BoardManager.getInstance().getBoardInstance().getPieceOn(prev)!=null)){
+            if (!(BoardManager.getInstance().getBoardInstance().getPieceOn(post).getType() == ChessPieceEnum.king)) {
 
-            ArrayList<Coord> isKingThere = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(post)).getMoveableList(post);
-            //checked check
-            for(int i = 0; i< isKingThere.size(); i++){
-                if(BoardManager.getInstance().getBoardInstance().getPieceOn(isKingThere.get(i)).getType() == ChessPieceEnum.king) {
-                    ArrayList<Coord> KingLoc = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(isKingThere.get(i))).getMoveableList(isKingThere.get(i));
-                    for(int j = 0; j< KingLoc.size();j++){
-                        for(int k = 0;k<8;k++){
-                            for(int l = 0; l<8;l++) {
-                                Coord BoardCoord = new Coord(k, l);
-                                if(BoardManager.getInstance().getBoardInstance().getPieceOn(BoardCoord).getPlayer()!=BoardManager.getInstance().getBoardInstance().getPieceOn(KingLoc.get(j)).getPlayer()) {
-                                    ArrayList<Coord> CheckedOrNot = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(BoardCoord)).getMoveableList(BoardCoord);
-                                    for(int m = 0;m<KingLoc.size();m++){
-                                        for(int n = 0;n<CheckedOrNot.size();n++){
-                                            if(KingLoc.get(m)==CheckedOrNot.get(n)) {
-                                                //Cannot move to here
-                                                KingLoc.remove(m);
-                                                return false;
+                ArrayList<Coord> isKingThere = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(post)).getMoveableList(post);
+                //checked check
+                for (int i = 0; i < isKingThere.size(); i++) {
+                    if(BoardManager.getInstance().getBoardInstance().getPieceOn(isKingThere.get(i))!=null) {
+                        if (BoardManager.getInstance().getBoardInstance().getPieceOn(isKingThere.get(i)).getType() == ChessPieceEnum.king) {
+                            ArrayList<Coord> KingLoc = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(isKingThere.get(i))).getMoveableList(isKingThere.get(i));
+                            for (int j = 0; j < KingLoc.size(); j++) {
+                                for (int k = 0; k < 8; k++) {
+                                    for (int l = 0; l < 8; l++) {
+                                        Coord BoardCoord = new Coord(k, l);
+                                        if ((BoardManager.getInstance().getBoardInstance().getPieceOn(BoardCoord) != null)&&(BoardManager.getInstance().getBoardInstance().getPieceOn(KingLoc.get(j))!=null)) {
+                                            if (BoardManager.getInstance().getBoardInstance().getPieceOn(BoardCoord).getPlayer() != BoardManager.getInstance().getBoardInstance().getPieceOn(KingLoc.get(j)).getPlayer()) {
+                                                ArrayList<Coord> CheckedOrNot = moveCheckerFactory.createMoveChecker(BoardManager.getInstance().getBoardInstance().getPieceOn(BoardCoord)).getMoveableList(BoardCoord);
+                                                for (int m = 0; m < KingLoc.size(); m++) {
+                                                    for (int n = 0; n < CheckedOrNot.size(); n++) {
+                                                        if (KingLoc.get(m) == CheckedOrNot.get(n)) {
+                                                            //Cannot move to here
+                                                            KingLoc.remove(m);
+                                                            return false;
+                                                        }
+                                                    }
+                                                }
                                             }
                                         }
                                     }
                                 }
                             }
+
+                            if (KingLoc.size() == 0) {
+                                //CheckMate!!! exit
+                                return false;
+                            }
+
                         }
                     }
-
-                    if(KingLoc.size()==0){
-                        //CheckMate!!! exit
-                        //TODO
-                    }
-
                 }
+
             }
 
         }
         return true;
     }
+
+
 }
