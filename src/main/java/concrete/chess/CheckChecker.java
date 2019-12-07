ae2ba6a01bf8fd8d3656dd499ad85f6887eac760
cau <cau@cau>
2019-12-08 01:12:31 +0900

    make CheckChecker

---
+++ src/main/java/concrete/chess/CheckChecker.java
@@ -0,0 +1,14 @@
+package concrete.chess;
+
+import bean.Coord;
+import piece.Piece;
+
+public class CheckChecker {
+    public CheckChecker(){
+
+    }
+
+    public boolean isCheck(Piece king, Coord destination){
+        return false;
+    }
+}
