259222d00b6390d78f6e2da1a89bbf92191ca617
cau <cau@cau>
2019-12-07 19:12:42 +0900

    merged version

--- src/main/java/controller/BoardEventListner.java
+++ src/main/java/controller/BoardEventListner.java
@@ -6,7 +6,5 @@ import piece.Piece;
 public interface BoardEventListner {
     public void onKilled(Piece piece);
     public void onPromoted(Piece piece, SelectedListener selectedListener);
-
-    void onPromoted();
     public void onGameOver();
 }
