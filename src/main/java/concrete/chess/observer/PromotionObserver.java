e015e69c4369293c89acadddf8ce5af9fc3a49ef
cau <cau@cau>
2019-12-07 18:58:32 +0900

    done to make promote

--- src/main/java/concrete/chess/observer/PromotionObserver.java
+++ src/main/java/concrete/chess/observer/PromotionObserver.java
@@ -10,24 +10,28 @@ import observer.Observer;
 import piece.Piece;
 
 public class PromotionObserver implements Observer, SelectedListener{
+    private Piece lastPiece;
+    private Coord lastCoord;
+
     BoardEventListner boardEventListner;
     @Override
     public void setBoardEventListner(BoardEventListner boardEventListner){
         this.boardEventListner=boardEventListner;
-
-        //boardEventListner.onPromoted(); notify promotion to view
-        //then onSelected(Piece piece) called when user finish answer
     }
     @Override
     public void onSelect(Piece piece){
-
+        BoardManager.getInstance().getBoardInstance().kill(lastCoord);
+        BoardManager.getInstance().getBoardInstance().update(lastPiece,lastCoord);
+        lastCoord=null;
+        lastPiece=null;
     }
     @Override
     public void update(Coord prev, Coord post) {
         if(BoardManager.getInstance().getBoardInstance().getPieceOn(post).getType()== ChessPieceEnum.pawn){
             if(post.getRow()==(BoardManager.getInstance().getBoardInstance().getPieceOn(post).getPlayer()*(-7)+14)){
-                //promotion active
-                //TODO
+                lastPiece = BoardManager.getInstance().getBoardInstance().getPieceOn(post);
+                lastCoord = post;
+                boardEventListner.onPromoted(lastPiece,this);
             }
         }
     }
